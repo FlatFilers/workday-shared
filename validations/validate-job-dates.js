5b01ca5cb6211683dfb3925dc5d3707699d5c929
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-01 13:41:02 -0600

    Update validate-job-dates.js
    - Bullet proofs the validate-job-dates-code to avoid empty linked fields

--- validations/validate-job-dates.js
+++ validations/validate-job-dates.js
@@ -23,70 +23,94 @@ export const validateJobAndDates = (record) => {
 
     // If job title is not provided but code is, get it from the job sheet and add info message
     if (isNil(title) && isNotNil(code)) {
-      const linkedJob = record.getLinks('Job_Profile_Reference_ID');
-      const jobTitle = linkedJob[0]?.title;
-      record.set('Job_Title', jobTitle);
-      record.addInfo('Job_Title', 'Title pulled from job profile.');
+      const linkedJob = record.getLinks('Job_Profile_Reference_ID')
+      const jobTitle =
+        linkedJob && linkedJob.length > 0 ? linkedJob[0]?.title : null
+      record.set('Job_Title', jobTitle)
+      record.addInfo('Job_Title', 'Title pulled from job profile.')
     }
 
     // If job title is provided but code is not, get it from the job sheet and add info message
     if (isNil(code) && isNotNil(title)) {
-      const linkedJob = record.getLinks('Job_Title');
-      const jobCode = linkedJob[0]?.code;
-      record.set('Job_Profile_Reference_ID', jobCode);
-      record.addInfo('Job_Profile_Reference_ID', 'Code pulled from job profile.');
+      const linkedJob = record.getLinks('Job_Title')
+      const jobCode =
+        linkedJob && linkedJob.length > 0 ? linkedJob[0]?.code : null
+      record.set('Job_Profile_Reference_ID', jobCode)
+      record.addInfo(
+        'Job_Profile_Reference_ID',
+        'Code pulled from job profile.'
+      )
     }
 
     // if there is a Job Code and a Job Title, but those do not match to the same code/title pair from the linked Job Profile, surface a warning
     if (isNotNil(code) && isNotNil(title)) {
-      const linkedJobfromTitle = record.getLinks('Job_Title');
-      const jobCode = linkedJobfromTitle[0]?.code;
-      const linkedJobfromCode = record.getLinks('Job_Profile_Reference_ID');
-      const jobTitle = linkedJobfromCode[0]?.title;
+      const linkedJobfromTitle = record.getLinks('Job_Title')
+      const jobCode =
+        linkedJobfromTitle && linkedJobfromTitle.length > 0
+          ? linkedJobfromTitle[0]?.code
+          : null
+      const linkedJobfromCode = record.getLinks('Job_Profile_Reference_ID')
+      const jobTitle =
+        linkedJobfromCode && linkedJobfromCode.length > 0
+          ? linkedJobfromCode[0]?.title
+          : null
       if (jobCode !== code || jobTitle !== title) {
-        record.addWarning(['Job_Profile_Reference_ID', 'Job_Title'], 'Job code and title match different jobs from the list of codes you previously entered.');
+        record.addWarning(
+          ['Job_Profile_Reference_ID', 'Job_Title'],
+          'Job code and title match different jobs from the list of codes you previously entered.'
+        )
       }
     }
 
     // Warning + default if contingent worker employee does not have an employment end date
     if (empType === 'CW' || terminated === true) {
       if (isNil(endEmploymentDate)) {
-        const message = 'Temp or Terminated Employees must have an Employment End Date, auto-set to 2099-12-31'
-        record.addWarning('End_Employment_Date', message);
+        const message =
+          'Temp or Terminated Employees must have an Employment End Date, auto-set to 2099-12-31'
+        record.addWarning('End_Employment_Date', message)
         record.set('End_Employment_Date', '2099-12-31')
       }
     } else {
       // Error if employment end date is provided for non-temp employee
       if (isNotNil(endEmploymentDate)) {
-        const message = 'Employment End Date is only valid for Temp or Terminated Employees'
+        const message =
+          'Employment End Date is only valid for Temp or Terminated Employees'
         record.addError('End_Employment_Date', message)
       }
     }
 
     // get pay rate from Job Profile and validate against worker data provided
-    const codeLookup = record.getLinks('Job_Profile_Reference_ID');
-    const payRateFromCode = codeLookup[0]?.pay_rate_type
+    const codeLookup = record.getLinks('Job_Profile_Reference_ID')
+    const payRateFromCode =
+      codeLookup && codeLookup.length > 0 ? codeLookup[0]?.pay_rate_type : null
     if (!payRate) {
       record.set('Pay_Rate_Type_Reference_ID', payRateFromCode)
     } else {
       if (payRate !== payRateFromCode) {
-        const message = 'Pay Rate doesn\'t match the pay rate from the linked job code.'
+        const message =
+          "Pay Rate doesn't match the pay rate from the linked job code."
         record.addWarning('Pay_Rate_Type_Reference_ID', message)
       }
     }
 
     // make sure employee type aligns with subtype
-    const cwArray = ['Board_Member','Consultant','Contractor']
-    const eeArray = ['Regular','Temporary','Seasonal','Fixed_Term','Intern','Apprentice']
+    const cwArray = ['Board_Member', 'Consultant', 'Contractor']
+    const eeArray = [
+      'Regular',
+      'Temporary',
+      'Seasonal',
+      'Fixed_Term',
+      'Intern',
+      'Apprentice',
+    ]
     if (empType === 'CW' && !cwArray.includes(subType)) {
       const message = 'Subtype not valid for Contingent Worker.'
       record.addError('Employee_Type_Reference_ID', message)
-    } 
+    }
     if (empType === 'EE' && !eeArray.includes(subType)) {
       const message = 'Subtype not valid for Employee.'
       record.addError('Employee_Type_Reference_ID', message)
     }
-
   } catch (err) {
     // If an error occurs during execution of the function, add an error message to the record with the error details
     console.error(err)
