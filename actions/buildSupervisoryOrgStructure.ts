06046febabf49c4173add5e29a1f2f698d338fe7
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-02 10:15:18 -0600

    Sup Org Updates
    - attempts to make code more bulletproof & efficient

--- actions/buildSupervisoryOrgStructure.ts
+++ actions/buildSupervisoryOrgStructure.ts
@@ -1,6 +1,8 @@
+// Import necessary modules
 import { validateReportingStructure } from '../actions/validateReportingStructure'
 import api, { Flatfile } from '@flatfile/api'
 
+// Define a class for building the supervisory organizational structure
 export class SupervisoryOrgStructureBuilder {
   private readonly workbookId: string
   private readonly sheetId: string
@@ -8,160 +10,218 @@ export class SupervisoryOrgStructureBuilder {
   private readonly orderedManagerIds: string[] = []
 
   constructor(workbookId: string, sheetId: string) {
+    if (!workbookId || !sheetId) {
+      throw new Error('Invalid workbookId or sheetId')
+    }
     this.workbookId = workbookId
     this.sheetId = sheetId
   }
 
   public async buildSupervisoryOrgStructure() {
-    // Retrieve the necessary data and perform the steps
-    const allRecords = await this.getAllRecords()
-    const reportingErrors = validateReportingStructure(allRecords)
+    try {
+      // Retrieve the necessary data and perform the steps
+      const allRecords = await this.getAllRecords()
+      const reportingErrors = validateReportingStructure(allRecords)
 
-    if (reportingErrors.length > 0) {
-      console.error('Reporting structure is invalid:', reportingErrors)
-      return // Stop the process if reporting structure is invalid
-    }
-
-    // Step 1: Validate the reporting structure
-
-    this.extractManagerIds(allRecords)
-    this.orderReportingStructure()
-
-    // Step 2: Extract managerIds and order the reporting structure
-
-    const newSheetData = this.orderedManagerIds.map((employeeId) => {
-      const record = allRecords.find(
-        (r) => r.values.Applicant_ID.value === employeeId
-      )
-      console.log('Processing record:', record)
-
-      return {
-        managerId: record.values.Organization_Reference_ID.value,
-        manager_name: record.values.Legal_Full_Name.value,
-        location: record.values.Location_Reference_ID.value,
-        // Include other relevant fields as needed
+      if (reportingErrors.length > 0) {
+        console.error('Reporting structure is invalid:', reportingErrors)
+        // Instead of stopping the process, you can handle the errors gracefully here
+        // For example, you can collect the errors and continue with the valid data
+        // const validRecords = allRecords.filter(record => !reportingErrors.includes(record))
+        // continue with validRecords...
+        return
       }
-    })
 
-    // Step 3: Create the new sheet data with ordered managerIds
+      // Step 1: Validate the reporting structure
+      this.extractManagerIds(allRecords)
+      this.orderReportingStructure()
 
-    await this.insertDataToExistingSheet(newSheetData)
+      // Step 2: Extract managerIds and order the reporting structure
+      const recordMap = this.createRecordMap(allRecords)
+      const newSheetData = this.orderedManagerIds.map((employeeId) => {
+        const record = recordMap.get(employeeId)
+        console.log('Processing record:', record)
 
-    // Step 4: Insert the ordered data into the existing sheet
-  }
+        return {
+          managerId: record.values.Organization_Reference_ID.value,
+          manager_name: record.values.Legal_Full_Name.value,
+          location: record.values.Location_Reference_ID.value,
+          // Include other relevant fields as needed
+        }
+      })
 
-  private async getAllRecords(): Promise<Flatfile.RecordWithLinks[]> {
-    const recordsResponse = await api.records.get(this.sheetId, {
-      includeCounts: true,
-    })
+      // Step 3: Create the new sheet data with ordered managerIds
+      await this.insertDataToExistingSheet(allRecords, newSheetData)
 
-    // Handle pagination if needed and return all records
-
-    return recordsResponse?.data?.records ?? []
-  }
-
-  private extractManagerIds(records: Flatfile.RecordWithLinks[]) {
-    for (const record of records) {
-      const employeeId = String(record.values.Applicant_ID.value) // Convert to string
-      const managerId = String(record.values.Organization_Reference_ID.value) // Convert to string
-
-      console.log('Extracting employeeId:', employeeId)
-      console.log('Extracting managerId:', managerId)
-
-      this.managerMap.set(employeeId, managerId)
+      // Step 4: Insert the ordered data into the existing sheet
+    } catch (error) {
+      console.error(
+        'An error occurred while building the supervisory organizational structure:',
+        error
+      )
+      // You can handle the error or log it as needed
     }
   }
 
+  // Retrieve all records from the specified sheet
+  private async getAllRecords(): Promise<Flatfile.RecordWithLinks[]> {
+    try {
+      const recordsResponse = await api.records.get(this.sheetId, {
+        includeCounts: true,
+      })
+
+      // Handle pagination if needed and return all records
+      return recordsResponse?.data?.records ?? []
+    } catch (error) {
+      console.error('An error occurred while retrieving records:', error)
+      throw error // Rethrow the error to the caller
+    }
+  }
+
+  // Extract managerIds from records and store them in a map
+  private extractManagerIds(records: Flatfile.RecordWithLinks[]) {
+    try {
+      for (const record of records) {
+        const employeeId = String(record.values.Applicant_ID.value) // Convert to string
+        const managerId = String(record.values.Organization_Reference_ID.value) // Convert to string
+
+        console.log('Extracting employeeId:', employeeId)
+        console.log('Extracting managerId:', managerId)
+
+        this.managerMap.set(employeeId, managerId)
+      }
+    } catch (error) {
+      console.error('An error occurred while extracting manager IDs:', error)
+      throw error // Rethrow the error to the caller
+    }
+  }
+
+  // Create a map of records with employeeId as the key
+  private createRecordMap(records: Flatfile.RecordWithLinks[]) {
+    try {
+      const recordMap = new Map<string, Flatfile.RecordWithLinks>()
+      for (const record of records) {
+        const employeeId = String(record.values.Applicant_ID.value) // Convert to string
+        recordMap.set(employeeId, record)
+      }
+      return recordMap
+    } catch (error) {
+      console.error('An error occurred while creating the record map:', error)
+      throw error // Rethrow the error to the caller
+    }
+  }
+
+  // Order the reporting structure based on manager hierarchy levels
   private orderReportingStructure() {
-    const visited: Set<string> = new Set()
-    const stack: { employeeId: string; level: number }[] = []
+    try {
+      const visited: Set<string> = new Set()
+      const stack: { employeeId: string; level: number }[] = []
 
-    for (const employeeId of this.managerMap.keys()) {
-      if (!visited.has(employeeId)) {
-        // Push the record with blank superior manager to the stack with level 0
-        if (!this.managerMap.get(employeeId)) {
-          stack.push({ employeeId, level: 0 })
-        } else {
-          stack.push({ employeeId, level: -1 }) // Push other records with a negative level initially
-        }
-
-        while (stack.length > 0) {
-          const { employeeId, level } = stack.pop()!
-          const managerId = this.managerMap.get(employeeId)
-
-          if (managerId && !visited.has(managerId)) {
-            // Increment the level if the managerId is not blank
-            const nextLevel = managerId ? level + 1 : level
-
-            // Push the record with its corresponding level
-            stack.push({ employeeId: managerId, level: nextLevel })
+      for (const employeeId of this.managerMap.keys()) {
+        if (!visited.has(employeeId)) {
+          // Push the record with blank superior manager to the stack with level 0
+          if (!this.managerMap.get(employeeId)) {
+            stack.push({ employeeId, level: 0 })
+          } else {
+            stack.push({ employeeId, level: -1 }) // Push other records with a negative level initially
           }
 
-          visited.add(employeeId)
+          while (stack.length > 0) {
+            const { employeeId, level } = stack.pop()!
+            const managerId = this.managerMap.get(employeeId)
+
+            if (managerId && !visited.has(managerId)) {
+              // Increment the level if the managerId is not blank
+              const nextLevel = managerId ? level + 1 : level
+
+              // Push the record with its corresponding level
+              stack.push({ employeeId: managerId, level: nextLevel })
+            }
+
+            visited.add(employeeId)
+          }
         }
       }
+
+      // Sort the visited managerIds based on their levels in ascending order
+      this.orderedManagerIds.push(
+        ...Array.from(visited).sort((a, b) => {
+          const levelA = this.managerMap.has(a) ? -1 : 0 // Records with blank superior manager should be at the top
+          const levelB = this.managerMap.has(b) ? -1 : 0
+          return levelA - levelB
+        })
+      )
+    } catch (error) {
+      console.error(
+        'An error occurred while ordering the reporting structure:',
+        error
+      )
+      throw error // Rethrow the error to the caller
     }
-
-    // Sort the visited managerIds based on their levels in ascending order
-    const orderedByLevel = Array.from(visited).sort((a, b) => {
-      const levelA = this.managerMap.has(a) ? -1 : 0 // Records with blank superior manager should be at the top
-      const levelB = this.managerMap.has(b) ? -1 : 0
-      return levelA - levelB
-    })
-
-    // Push the ordered managerIds to the orderedManagerIds array
-    this.orderedManagerIds.push(...orderedByLevel)
   }
 
-  private async insertDataToExistingSheet(data: any[]) {
-    const sheetSlug = 'orgs' // Slug of the existing sheet "Supervisory Orgs"
+  // Insert data into the existing sheet
+  private async insertDataToExistingSheet(
+    records: Flatfile.RecordWithLinks[],
+    data: any[]
+  ) {
+    try {
+      const sheetSlug = 'orgs' // Slug of the existing sheet "Supervisory Orgs"
 
-    // Retrieve the workbook by its ID
-    const workbook = await api.workbooks.get(this.workbookId)
+      // Retrieve the workbook by its ID
+      const workbook = await api.workbooks.get(this.workbookId)
 
-    // Find the sheet with the specified slug in the workbook's sheets array
-    const sheet = workbook?.data?.sheets?.find(
-      (s) => s.config?.slug === sheetSlug
-    )
+      // Find the sheet with the specified slug in the workbook's sheets array
+      const sheet = workbook?.data?.sheets?.find(
+        (s) => s.config?.slug === sheetSlug
+      )
 
-    if (!sheet) {
-      console.error(`Sheet with slug "${sheetSlug}" not found.`)
-      return
-    }
-
-    const records = data.map((record) => ({
-      code: { value: `Sup_Org_${record.managerId}` },
-      manager_name: { value: record.manager_name },
-      manager_id: { value: record.managerId }, // Wrap managerId in an object
-      manager_position: { value: `P-${record.managerId}` },
-      superior_code: { value: this.managerMap.get(record.managerId) }, // Wrap superior_code in an object
-      location: { value: record.location },
-
-      // Include other relevant fields as needed
-    }))
-
-    // Sort the records based on the hierarchy structure
-    const sortedRecords = this.orderedManagerIds.map((employeeId) =>
-      records.find((record) => record.manager_id.value === employeeId)
-    )
-
-    // Filter out undefined values
-    const finalRecords = sortedRecords.filter((record) => record !== undefined)
-
-    // Leave the superior_code field blank if manager_id is equal to superior_code
-    finalRecords.forEach((record) => {
-      if (record.manager_id.value === record.superior_code.value) {
-        record.superior_code.value = '' // Update the value property of superior_code
-      } else {
-        const correspondingRecord = finalRecords.find(
-          (r) => r.manager_id.value === record.superior_code.value
-        )
-        if (correspondingRecord) {
-          record.superior_code.value = correspondingRecord.code.value
-        }
+      if (!sheet) {
+        throw new Error(`Sheet with slug "${sheetSlug}" not found.`)
       }
-    })
 
-    await api.records.insert(sheet.id, finalRecords) // Insert the data into the existing sheet
+      // Prepare the data records to be inserted
+      const recordsToInsert = data.map((record) => ({
+        code: { value: `Sup_Org_${record.managerId}` },
+        manager_name: { value: record.manager_name },
+        manager_id: { value: record.managerId }, // Wrap managerId in an object
+        manager_position: { value: `P-${record.managerId}` },
+        superior_code: { value: this.managerMap.get(record.managerId) }, // Wrap superior_code in an object
+        location: { value: record.location },
+        // Include other relevant fields as needed
+      }))
+
+      // Sort the records based on the hierarchy structure
+      const sortedRecords = this.orderedManagerIds.map((employeeId) =>
+        recordsToInsert.find((record) => record.manager_id.value === employeeId)
+      )
+
+      // Filter out undefined values
+      const finalRecords = sortedRecords.filter(
+        (record) => record !== undefined
+      )
+
+      // Leave the superior_code field blank if manager_id is equal to superior_code
+      finalRecords.forEach((record) => {
+        if (record.manager_id.value === record.superior_code.value) {
+          record.superior_code.value = '' // Update the value property of superior_code
+        } else {
+          const correspondingRecord = finalRecords.find(
+            (r) => r.manager_id.value === record.superior_code.value
+          )
+          if (correspondingRecord) {
+            record.superior_code.value = correspondingRecord.code.value
+          }
+        }
+      })
+
+      await api.records.insert(sheet.id, finalRecords) // Insert the data into the existing sheet
+    } catch (error) {
+      console.error(
+        'An error occurred while inserting data into the existing sheet:',
+        error
+      )
+      throw error // Rethrow the error to the caller
+    }
   }
 }
