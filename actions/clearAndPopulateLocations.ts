cdc9505cbb6c401c4c92eeb04b436fa3a7d4c88b
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-06 09:48:37 -0600

    Refactor code for refreshing locations data
    - Modified the clearAndPopulateLocations function to delete existing records from the locations sheet in batches of 100, improving efficiency.
    - Updated the code handling the refresh job for the locations sheet to incorporate the enhanced clearAndPopulateLocations function.
    Implemented error handling to handle exceptions during the job execution and provide appropriate feedback.
    - These changes optimize the process of refreshing the locations sheet, resulting in improved efficiency and error handling.
    - Updated bluepint to include action refreshLocationsData

---
+++ actions/clearAndPopulateLocations.ts
@@ -0,0 +1,80 @@
+import api from '@flatfile/api'
+import { authenticateAndFetchLocations } from '../reference_data/locations.js'
+
+async function clearAndPopulateLocations(event) {
+  try {
+    // Get the ID of the workbook
+    const workbookId = event.context.workbookId
+    // Retrieve the workbook details
+    const workbook = await api.workbooks.get(workbookId)
+    // Find the sheet with "locations" in its slug
+    const locationsSheet = workbook.data.sheets.find((s) =>
+      s.config.slug.includes('locations')
+    )
+
+    if (locationsSheet) {
+      console.log('Locations sheet found')
+      const locationsId = locationsSheet.id
+
+      console.log('Fetching location data...')
+      // Fetch location data from the source (e.g., API)
+      const locationData = await authenticateAndFetchLocations()
+
+      if (locationData) {
+        console.log('Location data fetched successfully')
+        console.log('Location Data:', locationData)
+
+        console.log('Deleting existing records from the locations sheet...')
+        // Get the existing records from the locations sheet
+        const existingRecords = await api.records.get(locationsId)
+        // Extract the record IDs to delete
+        const recordIdsToDelete = existingRecords.data.records.map(
+          (record) => record.id
+        )
+
+        // Check if there are any existing records to delete
+        if (recordIdsToDelete.length === 0) {
+          console.log('No existing records to delete.')
+        } else {
+          console.log(`Deleting ${recordIdsToDelete.length} records...`)
+          // Delete the existing records from the locations sheet in batches
+          const batchSize = 100 // Set the desired batch size
+          const totalBatches = Math.ceil(recordIdsToDelete.length / batchSize)
+          for (let i = 0; i < totalBatches; i++) {
+            const start = i * batchSize
+            const end = (i + 1) * batchSize
+            const batchIds = recordIdsToDelete.slice(start, end)
+            await api.records.delete(locationsId, { ids: batchIds })
+            console.log(`Batch ${i + 1}/${totalBatches} deleted.`)
+          }
+          console.log('All existing records deleted.')
+        }
+
+        // Prepare the request to insert the location data
+        const request = locationData.map(({ locationName, locationID }) => ({
+          name: { value: locationName },
+          id: { value: locationID },
+          // Include other fields if necessary
+        }))
+
+        console.log('Inserting location data...')
+        // Insert the location data into the locations sheet
+        const insertLocations = await api.records.insert(locationsId, request)
+        console.log('Location data inserted:', insertLocations)
+
+        console.log('All location data inserted.')
+      } else {
+        console.error('Error: Failed to fetch location data')
+      }
+    } else {
+      console.error('Error: Locations sheet not found')
+    }
+  } catch (error) {
+    console.error(
+      'Error occurred during clear and populate of locations:',
+      error
+    )
+  }
+}
+
+export { clearAndPopulateLocations }
