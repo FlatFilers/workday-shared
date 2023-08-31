1feac4665a560dd05b6f6ecf84f8f0b39e9d5dce
Colin Frederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-08-31 09:26:23 -0500

    Delete node_modules directory

--- node_modules/@flatfile/api/core/form-data-utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFormDataContentLength = void 0;
-var getFormDataContentLength_1 = require("./getFormDataContentLength");
-Object.defineProperty(exports, "getFormDataContentLength", { enumerable: true, get: function () { return getFormDataContentLength_1.getFormDataContentLength; } });
