4e3ddcab49d82feb5ffd0d8f466645216e5b4806
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-09-20 12:50:34 -0600

    Removes Files & Typo
    - Removes workbook files
    - Fixes typo on welcome-page

--- workflow/welcome-page.js
+++ workflow/welcome-page.js
@@ -1,15 +1,15 @@
-import api from "@flatfile/api";
+import api from '@flatfile/api'
 
 export function createPage(spaceId) {
-    return api.documents.create(spaceId, {
-        title: 'Getting Started',
-        body:
-          '# Welcome\n' +
-          '### We\'re so excited to welcome you to Workday!\n' +
-          "We've set up this data migration space to support your launch.\n" +
-          "Your teammates from Workday have been added to this space.\n" +
-          "Please reach out to your implementation partner if you have any questions.\n" +
-          "Otherwise, you can get started by uploading an Excel or CSV file by navigating to Files -> Uplaod file.\n" +
-          '---\n',
-      })
+  return api.documents.create(spaceId, {
+    title: 'Getting Started',
+    body:
+      '# Welcome\n' +
+      "### We're so excited to welcome you to Workday!\n" +
+      "We've set up this data migration space to support your launch.\n" +
+      'Your teammates from Workday have been added to this space.\n' +
+      'Please reach out to your implementation partner if you have any questions.\n' +
+      'Otherwise, you can get started by uploading an Excel or CSV file by navigating to Files -> Upload file.\n' +
+      '---\n',
+  })
 }
