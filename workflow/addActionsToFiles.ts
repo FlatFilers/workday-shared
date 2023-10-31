c72361974c7ff91230ab66e5a972288072a79022
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-10-31 08:45:25 -0500

    Implement file action and decryption feature
    This commit introduces the functionality to add actions to files and subsequently decrypt those files via the introduced actions.
    
    - A new module has been added to handle file actions, providing a clear and concise API for interaction.
    - The decryption logic has been encapsulated within a separate function, ensuring modularity and ease of maintenance.
    
    This feature enhances the application’s ability to interact with encrypted files, providing a seamless user experience while maintaining data security.

---
+++ workflow/addActionsToFiles.ts
@@ -0,0 +1,28 @@
+import { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
+import api from '@flatfile/api'
+
+export default function addActionsToFiles(listener: FlatfileListener) {
+  listener.on(
+    'file:created',
+    async ({ context: { fileId } }: FlatfileEvent) => {
+      const file = await api.files.get(fileId)
+      const actions = file.data?.actions || []
+      const newActions = [
+        ...actions,
+        {
+          operation: 'logFileContents',
+          label: 'Log File Metadata',
+          description: 'This will log the file metadata.',
+        },
+        {
+          operation: 'decryptAction',
+          label: 'Decrypt File',
+          description: 'This will create a new decrypted file.',
+        },
+      ]
+      await api.files.update(fileId, {
+        actions: newActions,
+      })
+    }
+  )
+}
